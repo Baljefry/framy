fd83ac477c73d407b8fcad577f0ddb6cdc08f6de
Baljefry <baljeffry@gmail.com>
2024-02-19 03:20:33 +0300

    Update framy_config.dart

--- framy_generator/lib/config/framy_config.dart
+++ framy_generator/lib/config/framy_config.dart
@@ -1,28 +1,28 @@
 class FramyConfig {
   /// Specified in FramyApp annotation
-  bool useDevicePreview;
+  bool? useDevicePreview;
 
   /// Specified in config yaml file
   /// wrap-with-devicepreview: true
-  bool wrapWithDevicePreview;
+  bool wrapWithDevicePreview = true;
 
   /// Specified in config yaml file
   /// wrap-with-scaffold: true
-  bool wrapWithScaffold;
+  bool wrapWithScaffold = true;
 
   /// Specified in config yaml file
   /// wrap-with-center: true
-  bool wrapWithCenter;
+  bool wrapWithCenter = true;
 
   /// Specified in config yaml file
   /// wrap-with-safearea: true
-  bool wrapWithSafeArea;
+  bool wrapWithSafeArea = true;
 
   /// Specified in config yaml file
   /// show-material-components: true
-  bool showMaterialComponents;
+  bool showMaterialComponents = true;
 
   /// Specified in config yaml file
   /// show-storyboard: true
-  bool showStoryboard;
+  bool showStoryboard = true;
 }
