6636b0c42af5e4398844fa859e42527cb00043d3
Baljefry <baljeffry@gmail.com>
2024-02-19 03:32:17 +0300

    Update presets_map_generator.dart

--- framy_generator/lib/generator/presets_map_generator.dart
+++ framy_generator/lib/generator/presets_map_generator.dart
@@ -5,18 +5,18 @@ String generatePresets(List<FramyObject> presets) {
   final presetsByType = <String, List<FramyObject>>{};
   presets.forEach((element) {
     if (presetsByType.containsKey(element.returnType)) {
-      presetsByType[element.returnType].add(element);
+      presetsByType[element.returnType]?.add(element);
     } else {
       presetsByType[element.returnType] = [element];
     }
   });
 
   final presetsString = presetsByType.entries.fold(
-      '', (p, entry) => p + _generatePresetsForType(entry.key, entry.value));
+      '', (p, entry) => p.toString() + _generatePresetsForType(entry.key, entry.value));
 
   return 'Map<String, Map<String, dynamic>> createFramyPresets() => {$presetsString};';
 }
 
 String _generatePresetsForType(String type, List<FramyObject> presets) {
-  return '\'$type\': {${presets.fold('', (p, presetObject) => p + '\'${presetObject.name}\': ${generateAccessibleElement(presetObject)},')}},';
+  return '\'$type\': {${presets.fold('', (p, presetObject) => p.toString() + '\'${presetObject.name}\': ${generateAccessibleElement(presetObject)},')}},';
 }
